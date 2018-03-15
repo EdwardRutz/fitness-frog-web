e66de43c2a842648d240cd56064593fa01a1dc4b
EdwardRutz <96d7cf74@opayq.com>
2018-03-15 14:32:11 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# fitness-frog-web
+# Fitness Frog Web App
+
 C#, ASP.NET MVC fitness tracking app.
+
+----------------------------------------------------
+
+## Specs
+
+- Forms for CRUD Applications
+- Handling Form Posts
+- Model State
+- Using HTML Helpers to Create Form Fields
+- Model Binding
+- Client-Side and Server-Side Form Validation
+
+- Based on this [tutorial](https://teamtreehouse.com/library/aspnet-mvc-forms)
+
+
+
+## Dependencies
+
+- ASP.NET 4.6
--- src/Treehouse.FitnessFrog/Properties/AssemblyInfo.cs
+++ src/Treehouse.FitnessFrog/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FitnessFrogWeb")]
+[assembly: AssemblyTitle("Treehouse.FitnessFrog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("FitnessFrogWeb")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Treehouse.FitnessFrog")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86380890-3577-47cc-87e9-67a713c3497f")]
+[assembly: Guid("d6f7e536-7c34-46fc-a421-3069c9695d61")]
 
 // Version information for an assembly consists of the following four values:
 //
